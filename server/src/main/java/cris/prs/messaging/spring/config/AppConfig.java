68713103f280562223b4c9a4ca9649d9cd12edb2
Anupam Basak <basak.anupam@cris.org.in>
2026-09-10 13:40:01 +0530

    update

--- server/src/main/java/cris/prs/messaging/spring/config/AppConfig.java
+++ server/src/main/java/cris/prs/messaging/spring/config/AppConfig.java
@@ -1,9 +1,7 @@
 package cris.prs.messaging.spring.config;
 
-import org.cris.prs.messaging.solace.annotation.EnableSolace;
 import org.springframework.context.annotation.Configuration;
 
-@EnableSolace
 @Configuration
 public class AppConfig {
 
